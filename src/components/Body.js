ea686e0fb0e474e418e734a4d19a794ad8024cfe
vedesh2000 <vedeshkompella@gmail.com>
2023-11-19 17:04:04 +0530

    Done with ep-06 till real data fetch and search

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,53 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
-import resList from "../utils/mockData";
+import Shimmer from "./Shimmer";
+import { useEffect, useState } from "react";
 
 const Body = () => {
-  const [listofRestaurants , setlistofRestaurant] = useState(resList);
-  return (
+  const [listofRestaurants, setlistofRestaurant] = useState([]);
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=16.7106604&lng=81.0952431&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+    setlistofRestaurant(
+      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestaurants(
+      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  return listofRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              const filteredRestaurants = listofRestaurants.filter((res) => res.info.name.toLowerCase().includes(searchText.toLowerCase()))
+              setFilteredRestaurants(filteredRestaurants);
+              // console.log(listofRestaurants[0].info.name);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
@@ -14,14 +55,14 @@ const Body = () => {
             let filteredRestaurants = listofRestaurants.filter(
               (res) => res.info.avgRating > 4
             );
-            setlistofRestaurant(filteredRestaurants);
+            setFilteredRestaurants(filteredRestaurants);
           }}
         >
           Top Rated Restaurants
         </button>
       </div>
       <div className="res-container">
-        {listofRestaurants.map((restaurant) => (
+        {filteredRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,30 @@
-import {LOGO_URL} from "../utils/constants"
-
+import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
 const Header = () => {
-    return (
-      <div className="header">
-        <div className="logo-container">
-          <img
-            className="logo"
-            src={LOGO_URL}
-          />
-        </div>
-        <div className="nav-items">
-          <ul>
-            <li>Home</li>
-            <li>About us</li>
-            <li>Contact us</li>
-            <li>Cart</li>
-          </ul>
-        </div>
+  const [btname, setBtname] = useState("Login");
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={LOGO_URL} />
       </div>
-    );
-  };
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Contact us</li>
+          <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              btname === "Login" ? setBtname("Logout") : setBtname("Login");
+            }}
+          >
+            {btname}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-  export default Header;
+export default Header;
